f0d12d2f95e9d2482ae1cde52bbdd8396a694f2c
Hackbright Student <info@hackbrightacademy.com>
2016-07-19 12:41:22 -0700

    Modified nice.py to let users reach /hello by clicking a link and select a compliment from a dropdown menu. Upon submitting, uses chosen compliment with user's name.

--- nice.py
+++ nice.py
@@ -16,7 +16,18 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "Hi! This is the home page."
+    return """
+              <!doctype html>
+              <html>
+                <head>
+                  <title>Home Page</title>
+                </head>
+                <body>
+                  <h1>Hi! This is the home page.</h1>
+                  <a href="/hello">Click to be greeted with compliments...</a>
+                </body>
+              </html>
+           """
 
 
 @app.route('/hello')
@@ -33,6 +44,21 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           <label>What's your name? <input type="text" name="person"></label>
+          <label><select name = "compliment"> 
+                   <option value ="awesome">Awesome</option>
+                   <option value ="terrific">Terrific</option>
+                   <option value ="fantastic">Fantastic</option>
+                   <option value ="neato">Neato</option>
+                   <option value ="fantabulous">Fantabulous</option>
+                   <option value ="wowza">Wowza</option>
+                   <option value ="oh-so-not-meh">Oh-so-not-meh</option>
+                   <option value ="brilliant">Brilliant</option>
+                   <option value ="ducky">Ducky</option>
+                   <option value ="incredible">Incredible</option>
+                   <option value ="wonderful">Wonderful</option>
+                   <option value ="smashing">Smashing</option>
+                   <option value ="lovely">Lovely</option>
+          </select></label>
           <input type="submit">
         </form>
       </body>
@@ -46,7 +72,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
     return """
     <!doctype html>
